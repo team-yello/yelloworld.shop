aa4e868f76d4b393ed840c5b664addbf22330137
이의제 <j@changtalk.kr>
2023-09-03 04:04:32 +0900

    chore: export 제거

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "",
 };
 
 module.exports = nextConfig;
