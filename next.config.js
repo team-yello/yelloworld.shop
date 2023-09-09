9975ec948107e5acb7abe846a16eebe2e8099ccc
이의제 <j@changtalk.kr>
2023-09-09 21:00:07 +0900

    gh-page 세팅

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "http://yelloworld.shop/" : "",
 };
 
 module.exports = nextConfig;
